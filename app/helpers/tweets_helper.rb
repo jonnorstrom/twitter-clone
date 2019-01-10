54e1c63a97b7122a6c6bc6ed153d77a18b082a79
Jonathan Norstrom <jon.norstrom@gmail.com>
2019-01-09 19:30:24 -0500

    add tags

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,25 @@
 module TweetsHelper
+  def get_tagged(tweet)
+    message_array = tweet.message.split
+
+    message_array.each_with_index do |word, index|
+        if word[0] == "#"
+          if Tag.pluck(:phrase).include?(word)
+            tag = Tag.find_by(phrase: word)
+          else
+            tag = Tag.create({phrase: word})
+          end
+
+          # tag = Tag.find_or_create_by(phrase: word)
+
+          ## Tag.where(phrase: word) -> if yes, save variable
+          ## if no, make Tag object in db, then get new id from it
+          tt = TweetTag.create(tag_id: tag.id, tweet_id: tweet.id)
+          message_array[index] = "<a href='/tag_tweets?id=#{tag.id}'>#{word}</a>"
+        end
+    end
+
+    tweet.update(message: message_array.join(" "))
+    return tweet
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,22 @@ class Tweet < ApplicationRecord
     validates :message, presence: true
     validates :message, length: {maximum: 140, 
 too_long: "A tweet is only 140 max. Everybody knows that!"}
+
+    has_many :tweet_tags
+    has_many :tags, through: :tweet_tags
+
+
+    ## Tweet.find(1) == @tweet
+    # @tweet.tags -> [Tag, Tag, Tag, Tag]
+    # def tags
+    #     tts = TweetTag.where(tweet_id: @tweet.id)
+    #     # -> [TweetTag, TweetTag, TweetTag, TweetTag]
+    #     # @tweetTag -> tweet_id, tag_id
+    #     tags = []
+    #     tts.each do |tweetTag|
+    #         tags.push(Tag.find_by(id: tweetTag.tag_id))
+    #     end 
+
+    #     return tags # -> [Tag, Tag, Tag]
+    # end
 end
